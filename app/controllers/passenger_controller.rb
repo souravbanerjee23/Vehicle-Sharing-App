25fe03f7af8034d44acf4398af6e9daafde53947
Sourav Bandyopadhyay <souravbandyopadhyay1997@gmail.com>
2021-11-21 17:50:31 +0530

    Change in passenger controller

--- app/controllers/passenger_controller.rb
+++ app/controllers/passenger_controller.rb
@@ -1,24 +1,2 @@
 class PassengerController < ApplicationController
-  def login
-    @passenger=Passenger.all
-  end
-  def show
-    @passenger = Passenger.find(params[:id])
-  end
-
-  def new
-    @passenger = Passenger.new
-  end
-
-  def create
-    @passenger = Passenger.new(passenger_id: "...", phone: "...",address: "...",email:"...",password:"...")
-
-    if @article.save
-      redirect_to @passenger
-    else
-      render :new
-    end
-  end
 end
-end
- 
